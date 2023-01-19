a288444eb61694cf215a20ed9e06985f45f80775
Bryce Bohlman <S1457378@student.mcckc.edu>
2023-01-18 20:47:39 -0600

    Adding all project files

---
+++ csis265proj05/CoffeeMaker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csis265proj05
+{
+    public class CoffeeMaker
+    {
+        private string brandName;
+        private double tankCapacityInOunces;
+        private bool filterContainerInPlace;
+        private bool powerOn;
+
+        #region CONSTRUCTORS
+        public CoffeeMaker()
+        {
+            brandName = string.Empty;
+            tankCapacityInOunces = 0;
+            filterContainerInPlace = false;
+            powerOn = false;
+        }
+
+        public CoffeeMaker(string brand, double capacity, bool basketPresent, bool isOn)
+        {
+            brandName = brand;
+            tankCapacityInOunces = capacity;
+            filterContainerInPlace = basketPresent;
+            powerOn = isOn;
+        }
+        #endregion
+
+        #region SETTERS
+        public void SetBrandName(string inputBrandName)
+        {
+            brandName = inputBrandName;
+        }
+        public void SetTankCapacityInOunces(double inputTankCapacityInOunces)
+        {
+            tankCapacityInOunces = inputTankCapacityInOunces;
+        }
+        public void SetFilterContainerInPlace(bool inputFilterContainerInPlace)
+        {
+            filterContainerInPlace = inputFilterContainerInPlace;
+        }
+        public void SetPowerOn(bool inputPowerOn)
+        {
+            powerOn = inputPowerOn;
+        }
+        #endregion
+
+        #region GETTERS
+        public string GetBrandName()
+        {
+            return brandName;
+        }
+        public double GetTankCapacityInOunces()
+        {
+            return tankCapacityInOunces;
+        }
+        public bool GetFilterContainerInPlace()
+        {
+            return filterContainerInPlace;
+        }
+        public bool GetPowerOn()
+        {
+            return powerOn;
+        }
+        #endregion
+
+        public void BrewCoffee()
+        {
+            System.Console.WriteLine($"Machine brand is: {GetBrandName()}");
+            System.Console.WriteLine($"Reservoir Capacity is: {GetTankCapacityInOunces()}");
+            System.Console.WriteLine($"Switch is: {GetPowerOn()}");
+            System.Console.WriteLine($"Basket is: {GetFilterContainerInPlace()}\n");
+
+            if (filterContainerInPlace && powerOn)
+            {
+                System.Console.WriteLine($"Brewing coffee using {GetTankCapacityInOunces()} ounces of water.");
+            }
+            else
+            {
+                System.Console.WriteLine($"Cannot brew coffee!!! BASKET: {GetFilterContainerInPlace()} SWITCH: {GetPowerOn()}");//In the example, there was no colon after SWITCH, 
+            }                                                                                                                   //however, I included one anyway because its absence 
+        }                                                                                                                       //bothered me and I assumed it was an oversight
+    }
+}
---
+++ csis265proj05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csis265proj05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CoffeeMaker myCoffeeMachine = new CoffeeMaker();
+
+
+            myCoffeeMachine.SetBrandName("Keuregano");
+            myCoffeeMachine.SetTankCapacityInOunces(124.62);
+            myCoffeeMachine.SetFilterContainerInPlace(false);
+            myCoffeeMachine.SetPowerOn(true);
+
+
+            myCoffeeMachine.BrewCoffee();
+
+            System.Console.ReadLine();
+        }
+    }
+}
